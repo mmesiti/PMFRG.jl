174660092e3c7a7f25007870b94632118f9afe78
Michele Mesiti <michele.mesiti@kit.edu>
2024-02-23 02:21:48 +0100

    Docs building locally
    Still tring to understand deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,7 @@
-using Pkg
-Pkg.activate()
-using Documenter
-Pkg.activate("../")
-push!(LOAD_PATH,"../src/")
+using Documenter, PMFRG
+#push!(LOAD_PATH,"../src/")
 
-using PMFRG
-makedocs(sitename = "PMFRG.jl",modules  = [PMFRG],pages=["Home" => "index.md"])
-deploydocs(;repo="git@gitlabph.physik.fu-berlin.de:niggeni/PMFRG.jl",)
+makedocs(sitename="PMFRG.jl", repo=Remotes.GitHub("mmesiti","PMFRG.jl"))
+#deploydocs(;repo="git@gitlabph.physik.fu-berlin.de:niggeni/PMFRG.jl",)
+deploydocs(;repo="github.com/mmesiti/PMFRG.jl.git",)
 # deploydocs(;repo="github.com/NilsNiggemann/PMFRG.jl",)
